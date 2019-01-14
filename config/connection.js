8a9f384c59faeabf9ad88d0451f11a62e3743e97
Matthew Abrahamson <mra.abrahamson@gmail.com>
2019-01-14 13:13:29 -0800

    working on jawsdb integration

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  // port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Colbert1070",
-  database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Colbert1070'
+    database: 'burgers_db'
+  });
+};
+
 
 // Make connection.
 connection.connect(function(err) {
