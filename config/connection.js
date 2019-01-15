475b224b2933af5e0df10b6ce9cdaa6009ce8802
Matthew Abrahamson <mra.abrahamson@gmail.com>
2019-01-15 14:48:34 -0800

    updating connections.js

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,7 @@
 // var connection;
 
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWS_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Colbert1070',
-//     database: 'burgers_db'
-//   });
-// };
+
 
 
 // // Make connection.
@@ -31,37 +22,51 @@
 // *********************************************************************************
 
 var mysql = require("mysql");
+var connection;
 
-// we placed the connections in this source object
-var source = {
-  // localhost
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Colbert1070",
-    database: "burgers_db"
-  },
 
-  // jawsDB
-  jawsDB: {
-    host: "	ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "ybt731luevb62k87",
-    password: "dtd1pp7uhcvkjvon",
-    database: "arbml24pcr5btyow"
-  }
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Colbert1070',
+    database: 'burgers_db'
+  });
 };
 
+// // we placed the connections in this source object
+// var source = {
+//   // localhost
+//   localhost: {
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Colbert1070",
+//     database: "burgers_db"
+//   },
+
+//   // jawsDB
+//   jawsDB: {
+//     host: "	ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "ybt731luevb62k87",
+//     password: "dtd1pp7uhcvkjvon",
+//     database: "arbml24pcr5btyow"
+//   }
+// };
+
 // we use source.[name of connection] to hook into mysql
-var connection = mysql.createConnection(source.localhost);
+// var connection = mysql.createConnection(source.localhost);
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+connection.connect();
 module.exports = connection;
